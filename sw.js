421741ecf55af3b461954d740f1f4c95840fae70
Edferal1 <37854287+Edferal1@users.noreply.github.com>
2026-03-01 18:14:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,28 @@
-// Este arquivo permite que o app funcione offline no futuro.
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js")
-    .then(() => console.log("SW registrado"))
-    .catch(err => console.log("Erro:", err));
-}
+const CACHE_NAME = "jokenpo-v1";
+
+self.addEventListener("install", event => {
+    console.log("Service Worker instalado");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./manifest.json",
+                "./icon-192.png",
+                "./icon-512.png",
+                "./pc.png",
+                "./pcpedra.png",
+                "./pcpapel.png",
+                "./pctesoura.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
