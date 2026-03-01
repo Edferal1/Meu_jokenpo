a63324369ca5111b1eaf200900786fe5d4c66244
Edferal1 <37854287+Edferal1@users.noreply.github.com>
2026-03-01 18:47:49 -0300

    Delete serviceworker.js

--- serviceworker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('Service Worker registrado com o escopo:', registration.scope);
-    }, function(err) {
-      console.log('Registro do Service Worker falhou:', err);
-    });
-  });
-}
